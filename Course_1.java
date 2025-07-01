e9f890adaf2e848cdfbd0ccfe521ef5886db2f3a
trey_weathers <treychelle.weathers2017@gmail.com>
2025-07-01 18:05:17 -0400

    Update Course_1.java

--- Course_1.java
+++ Course_1.java
@@ -1,6 +1,6 @@
 //COURSE 1.1: Getting Started with Java
 //LESSON 1: Java Basics: Syntax, Comments, Semicolons, and Your First Program
-//Practice # 1
+//Practice #1
     class Solution {
         public static void main(String[] args) {
             // This command prints a message to the console
@@ -8,35 +8,34 @@
         }
     }
 //Practice #2: Change the text within the System.out.println to "Journey to the stars begins with Java!"
-    class Solution {
+    class Solution2 {
         public static void main(String[] args) {
             // This line prints a message about space exploration
             System.out.println("Journey to the stars begins with Java!"); // Please change the message!
         }
     }
 //Practice #3: Find the bug
-    class Solution {
+    class Solution3 {
         public static void main(String[] args) {
             // Sending a greeting to interstellar explorers
             System.out.println("Greetings, interstellar explorers!")
         }
     }
-
-
+/*
     solution.java:4: error: ';' expected
             System.out.println("Greetings, interstellar explorers!")
                                                                 ^
     1 error
+*/
 
-
-    class Solution {
+    class Solution4 {
         public static void main(String[] args) {
             // Sending a greeting to interstellar explorers
             System.out.println("Greetings, interstellar explorers!");
         }
     }
 //Practice #4: Complete the code to broadcast your greeting across the cosmos!
-    class Solution {
+    class Solution5 {
         public static void main(String[] args) {
             // TODO: Print a welcome message for new space explorers learning Java.
             System.out.println("Welcome new space explorers learning Java!");
@@ -45,7 +44,7 @@
 //Practice #5: Start the countdown for the launch of our virtual spacecraft. Remember, every Java program starts with a class and a main method.
     // TODO: Declare a class named 'Solution' (other class names won't work)
     // Make sure NOT to make your class 'public'
-    class Solution {
+    class Solution6 {
          // TODO: Inside your class, define the main 'public static void main' method that Java looks for
          public static void main(String[] args) {
             // TODO: Use a print statement to display a message "3, 2, 1... Lift off!" to the console
@@ -55,7 +54,7 @@
 
 //LESSON 2: Java Variables: Storing Data on Our Journey to the Stars
 //Practice #1
-    class Solution {
+    class Solution7 {
         public static void main(String[] args) {
             final int SOLAR_SYSTEM_PLANETS = 8;
             System.out.println("Number of planets in the Solar System: " + SOLAR_SYSTEM_PLANETS);
@@ -65,7 +64,7 @@
         }
     }
 //Practice #2: Modify the provided starter code to define the speed of light as a constant that does not change. Remember to follow the proper Java syntax for constants.
-    class Solution {
+    class Solution8 {
         public static void main(String[] args) {
             // Creating a variable for representing the speed of light
             final int speedOfLightKmS = 299792;
@@ -75,7 +74,7 @@
         }
     }
 //Practice #3: Your mission is to find and fix the bug. Happy debugging!
-    class Solution {
+    class Solution9 {
         public static void main(String[] args) {
             int earthOrbitRadius = 149600000; // in kilometers
             System.out.println("The Earth's orbit radius is " + earthOrbitRadius + " kilometers.");
@@ -85,7 +84,7 @@
         }
     }
 //Practice #4: It's time to define a constant for our cosmic journey speed. Add the necessary code to ensure our spaceship's dashboard displays all the crucial information.
-    class Solution {
+    class Solution10 {
         public static void main(String[] args) {
             // TODO: Declare a constant for Earth's orbital speed and set its value to 29 km/s
             final int EARTHS_ORBITAL_SPEED = 29;
@@ -94,7 +93,7 @@
         }
     }
 //Practice #5: Let's recap the variables and constants related to our cosmic journey. Create a new constant to represent Earth's orbital speed and a variable for the number of planets, then display their values. Ready to blast off?
-    class Solution {
+    class Solution11 {
         public static void main(String[] args) {
             // TODO: Declare a constant for Earth's orbital speed and give it a value of 108000 (km/h)
             final int EARTHS_ORBITAL_SPEED = 108000;
