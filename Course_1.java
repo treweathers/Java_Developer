0ad7f94d4b919304e64c624f7896b2b823c6780b
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2025-07-01 15:37:46 -0400

    Update Course_1.java

--- Course_1.java
+++ Course_1.java
@@ -1,4 +1,88 @@
 //Course 1: Java Programming for Beginners
+//Practice #1
+    class Solution {
+        public static void main(String[] args) {
+            final int SOLAR_SYSTEM_PLANETS = 8;
+            System.out.println("Number of planets in the Solar System: " + SOLAR_SYSTEM_PLANETS);
+            
+            int universeAge = 14;
+            System.out.println("The universe is approximately " + universeAge + " billion years old.");
+        }
+    }
+
+//Practice #2: Modify the provided starter code to define the speed of light as a constant that does not change. Remember to follow the proper Java syntax for constants.
+    class Solution {
+        public static void main(String[] args) {
+            // Creating a variable for representing the speed of light
+            int speedOfLightKmS = 299792;
+    
+            // Output the speed of light
+            System.out.println("The speed of light is: " + speedOfLightKmS);
+        }
+    }
+    
+    class Solution {
+        public static void main(String[] args) {
+            // Creating a variable for representing the speed of light
+            final int speedOfLightKmS = 299792;
+    
+            // Output the speed of light
+            System.out.println("The speed of light is: " + speedOfLightKmS);
+        }
+    }
+
+//Practice #3: Your mission is to find and fix the bug. Happy debugging!
+    class Solution {
+        public static void main(String[] args) {
+            int earthOrbitRadius = 149600000; // in kilometers
+            System.out.println("The Earth's orbit radius is " + earthOrbitRadius + " kilometers.");
+    
+            int 8planets = 8;
+            System.out.println("There are " + 8planets + " planets in the Solar System.");
+        }
+    }
+
+    class Solution {
+        public static void main(String[] args) {
+            int earthOrbitRadius = 149600000; // in kilometers
+            System.out.println("The Earth's orbit radius is " + earthOrbitRadius + " kilometers.");
+    
+            int planets8 = 8;
+            System.out.println("There are " + planets8 + " planets in the Solar System.");
+        }
+    }
+
+//Practice #4: It's time to define a constant for our cosmic journey speed. Add the necessary code to ensure our spaceship's dashboard displays all the crucial information.
+    class Solution {
+        public static void main(String[] args) {
+            // TODO: Declare a constant for Earth's orbital speed and set its value to 29 km/s
+            int planetsInSolarSystem = 8; // Number of planets in the solar system
+            // TODO: Print out the string "Earth's orbital speed: <speed value> km/s
+            System.out.println("Number of planets: " + planetsInSolarSystem);
+        }
+    }
+
+    class Solution {
+        public static void main(String[] args) {
+            // TODO: Declare a constant for Earth's orbital speed and set its value to 29 km/s
+            final int EARTHS_ORBITAL_SPEED = 29;
+            // TODO: Print out the string "Earth's orbital speed: <speed value> km/s
+            System.out.println("Earth's orbital speed: " + EARTHS_ORBITAL_SPEED + "km/s");
+        }
+    }
+
+//Practice #5: Let's recap the variables and constants related to our cosmic journey. Create a new constant to represent Earth's orbital speed and a variable for the number of planets, then display their values. Ready to blast off?
+    class Solution {
+        public static void main(String[] args) {
+            // TODO: Declare a constant for Earth's orbital speed and give it a value of 108000 (km/h)
+            final int EARTHS_ORBITAL_SPEED = 108000;
+            // TODO: Declare a variable for the number of planets in the Solar System and assign it to 8
+            int planetsInSolarSystem = 8;
+            // TODO: Print out both the orbital speed of the Earth and the number of planets using print statements
+            System.out.println(EARTHS_ORBITAL_SPEED);
+            System.out.println(planetsInSolarSystem);
+        }
+    }
 
 
 //Course 1.1: Getting Started with Java
