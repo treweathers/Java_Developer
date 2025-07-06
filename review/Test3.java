21f7b5c06ce2b4fa3af0fa8b59d042f8bb467524
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2025-07-06 07:52:28 -0400

    Update Test3.java

--- review/Test3.java
+++ review/Test3.java
@@ -27,10 +27,10 @@ class Test3 {
         // While loop for counting down until liftoff
         int liftoffCount = 5;
         while (liftoffCount >= 0) { // TODO: Write the condition for continuing the countdown
-            System.out.println(liftoffCount + " seconds.");
-            liftoffCount--;
             // TODO: Print the log message with the current countdown status
+            System.out.println(liftoffCount + " seconds.");
             // TODO: What else needs to be done after each countdown print to prevent an infinite loop?
+            liftoffCount--;
         }
         
         // Do-while loop for simulating at least one orbit around a planet
